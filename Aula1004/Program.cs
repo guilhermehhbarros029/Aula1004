46c7b50a917ef29460b9445d76cfca00415d0d88
guilherme.hhbarros <guilherme.hhbarros@OSA0716349W11-1>
2026-04-15 22:06:31 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1004/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Aula1004;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Estruturas condicionais compostas
+
+        /*
+        double nota = 12;
+
+        if (nota < 0 || nota > 10)
+        {
+            Console.WriteLine("Essa nota NÃO é válida");
+        }
+        else if (nota < 5)
+        {
+            Console.WriteLine("Reprovado");
+        }
+        else if (nota < 7)
+        {
+            Console.WriteLine("Recuperação");
+        }
+        else
+        {
+            Console.WriteLine("Aprovado");
+        }
+
+        */
+
+        int opcao = 0;
+
+
+    }
+
+}        
